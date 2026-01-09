a09bf817aca3039f60a1900c007ae15df1e82b80
KoenMW <koen.wijchers@gmail.com>
2026-01-09 23:10:32 +0100

    feat: enhance user management with toast notifications for create, update, and delete actions

--- src/util/user.ts
+++ src/util/user.ts
@@ -2,6 +2,7 @@ import { get } from "svelte/store";
 import type { User } from "../types";
 import { rolesStore } from "../stores/roles";
 import { del, post, put } from "./api";
+import { AddToastPromise } from "./toast";
 
 export const GetUserRoleName = (user: User) => {
   const store = get(rolesStore);
@@ -15,20 +16,38 @@ export const DeleteUser = async (user: User) => {
   if (!confirm(`Are you sure you want to delete user ${user.name}?`)) {
     return { ok: false, error: "User deletion cancelled" };
   }
-  return await del(`${import.meta.env.VITE_USER_API_URL}users/${user.id}`);
+  const promise = del(`${import.meta.env.VITE_USER_API_URL}users/${user.id}`);
+  AddToastPromise(promise, {
+    loading: `Deleting user ${user.name}...`,
+    success: `User ${user.name} deleted successfully.`,
+    error: `Failed to delete user ${user.name}.`,
+  });
+  return await promise;
 };
 
 export const UpdateUser = async (user: User) => {
   user.role = GetUserRoleName(user);
 
-  return await put(
+  const promise = put(
     `${import.meta.env.VITE_USER_API_URL}users/${user.id}`,
     user
   );
+  AddToastPromise(promise, {
+    loading: `Updating user ${user.name}...`,
+    success: `User ${user.name} updated successfully.`,
+    error: `Failed to update user ${user.name}.`,
+  });
+  return await promise;
 };
 
 export const CreateUser = async (user: User) => {
   user.role = GetUserRoleName(user);
 
-  return await post(`${import.meta.env.VITE_USER_API_URL}users`, user);
+  const promise = post(`${import.meta.env.VITE_USER_API_URL}users`, user);
+  AddToastPromise(promise, {
+    loading: `Creating user ${user.name}...`,
+    success: `User ${user.name} created successfully.`,
+    error: `Failed to create user ${user.name}.`,
+  });
+  return await promise;
 };
