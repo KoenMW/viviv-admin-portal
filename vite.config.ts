1ec15df1ff10444a7b6ec5974d9afb69427d1a8d
Wojciech Stachowiak <>
2026-01-20 19:22:48 +0100

    restore original config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    proxy: {
-      "/auth": {
-        target: "http://localhost:9000",
-        changeOrigin: true
-      }
-    }
-  }
+  base: "/viviv-admin-portal",
 });
